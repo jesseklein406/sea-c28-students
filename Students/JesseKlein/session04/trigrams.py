fb65c266528c9e87d6f335260f219cad140b632f
Jesse Klein <jklein08@gmail.com>
2014-12-16 15:44:36 -0800

    Add files 'sherlock.txt' and 'trigrams.py'
    Add story 'sherlock.txt' and first draft of 'trigrams.py' to repo.

---
+++ Students/JesseKlein/session04/trigrams.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import io
+from random import random
+import math
+
+f = io.open("sherlock.txt", encoding="utf-8")
+story = f.read()
+story_list = story.split()
+
+story_dict = {}
+
+for i in xrange(len(story_list[:])):
+    key_words = u" ".join(story_list[:2])
+    if len(story_list) == 1:
+        key_words += u" lorem"
+    story_dict[key_words] = story_dict.get(key_words, [])
+    if len(story_list) == 2:
+        story_dict[key_words].append(u"lorem")
+    elif len(story_list) == 1:
+        story_dict[key_words].append(u"ipsum")
+    else:
+        story_dict[key_words].append(story_list[2])
+    story_list.pop(0)
+
+new_story = u"So there I was"
+new_story_list = new_story.split()
+
+while True:
+    key_words = u" ".join(new_story_list[-2:])
+    if key_words == u"lorem ipsum":
+        break
+    new_story_list.append(story_dict[key_words][int(math.floor(random() * len(story_dict[key_words])))])
+
+new_story = u" ".join(new_story_list) + u" -- THE END"
+
+new_story_file = io.open("new_story.txt", "w", encoding="utf-8")
+new_story_file.write(new_story)
+
