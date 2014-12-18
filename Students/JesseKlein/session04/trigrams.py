8b4b7d9991e92ffaf6af8e4d72843080a30f9ae7
Jesse Klein <jklein08@gmail.com>
2014-12-17 16:17:05 -0800

    Combine unnecessary lines of code in 'trigrams.py'
    Combine for loops and delete unnecessary code in 'trigrams.py'

--- Students/JesseKlein/session04/trigrams.py
+++ Students/JesseKlein/session04/trigrams.py
@@ -11,14 +11,24 @@ story_list = story.split(u" ")
 story_dict = {}
 
 for i in xrange(len(story_list)):
-    key_words = u" ".join(story_list[i:(i + 2)])
-    if i < (len(story_list) - 2):
-        story_dict.setdefault(key_words, []).append(story_list[i + 2])
+    key_words_3_gram = u" ".join(story_list[i:(i + 2)])
+    key_words_4_gram = u" ".join(story_list[i:(i + 3)])
+    if i < (len(story_list) - 3):
+        story_dict.setdefault(key_words_3_gram, []).append(story_list[i + 2])
+        story_dict.setdefault(key_words_4_gram, []).append(story_list[i + 3])
+    elif i == (len(story_list) - 3):
+        story_dict.setdefault(key_words_3_gram, []).append(story_list[i + 2])
+        story_dict.setdefault(key_words_4_gram, []).append(u"lorem")
     elif i == (len(story_list) - 2):
-        story_dict.setdefault(key_words, []).append(u"lorem")
+        story_dict.setdefault(key_words_3_gram, []).append(u"lorem")
+        key_words_4_gram += u" lorem"
+        story_dict.setdefault(key_words_4_gram, []).append(u"ipsum")
     else:
-        key_words += u" lorem"
-        story_dict.setdefault(key_words, []).append(u"ipsum")
+        key_words_3_gram += u" lorem"
+        story_dict.setdefault(key_words_3_gram, []).append(u"ipsum")
+        key_words_4_gram += u" lorem ipsum"
+        story_dict.setdefault(key_words_4_gram, []).append(u"dolor")
+
 
 new_story = u"So there I was doing Sherlock Holmes things when all of a sudden"
 new_story_list = new_story.split()
@@ -33,19 +43,6 @@ new_story = u" ".join(new_story_list) + u" -- THE END"
 
 new_story_file = io.open("new_story.txt", "w", encoding="utf-8")
 new_story_file.write(new_story)
-
-for i in xrange(len(story_list)):
-    key_words = u" ".join(story_list[i:(i + 3)])
-    if i < (len(story_list) - 3):
-        story_dict.setdefault(key_words, []).append(story_list[i + 3])
-    elif i == (len(story_list) - 3):
-        story_dict.setdefault(key_words, []).append(u"lorem")
-    elif i == (len(story_list) - 2):
-        key_words += u" lorem"
-        story_dict.setdefault(key_words, []).append(u"ipsum")
-    else:
-        key_words += u" lorem ipsum"
-        story_dict.setdefault(key_words, []).append(u"dolor")
 
 new_story = u"So there I was doing Sherlock Holmes things when all of a sudden"
 new_story_list = new_story.split()
