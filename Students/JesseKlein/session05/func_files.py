b1656c2cd6a299cae3830c7be30e93e01ffb89b1
Jesse Klein <jklein08@gmail.com>
2014-12-28 19:17:29 -0800

    Modify 'func_files.py'
    Add some commenting and docstrings to 'func_files.py'

--- Students/JesseKlein/session05/func_files.py
+++ Students/JesseKlein/session05/func_files.py
@@ -1,22 +1,32 @@
 #!/usr/bin/env python
-"""
+"""Run this module from the console. Add an argument for an input file.
+Use this module to remove leading and trailing whitespace from each line
+of a file's contents. Save the output to a new file and filename or
+overwrite the input file.
 """
 import io
 import sys
 filename = sys.argv[1]
 
 def clean(s):
+    """Remove leading and trailing whitespace from a string.
+
+    Enter a string as an argument. Return another string.
+    """
     list_s = s.split(u"\n")
-    strip_s = map(unicode, map(str.strip, map(str, list_s)))
+    strip_s = map(unicode, map(str.strip, map(str, list_s)))    # Assign modified list to another list
     cleaned_s = u"\n".join(strip_s)
     return cleaned_s
 
 
 f = io.open(filename, "r+", encoding="utf-8")
 f_string = f.read()
-new_f_string = map(clean, [f_string])[0]
+new_f_string = map(clean, [f_string])[0]    # Return the first and only item in the mapped list to get the new string
 
 c = raw_input(u"Please type 1 to create a new file or 2 to overwrite the existing one: ")
+
+while not c in ["1", "2"]:    # Validate the input
+    c = raw_input(u'That is not a "1" or a "2". Pleases type either 1 or 2: ')
 
 if c == "1":
     new_filename = raw_input(u"Please type a new filename: ")
@@ -25,9 +35,9 @@ if c == "1":
     new_f.close()
 
 if c == "2":
-    f.seek(0)
+    f.seek(0)    # Start the file over
     f.write(new_f_string)
-    f.truncate()
+    f.truncate()    # Trim off any old code
 
 f.close()
 
